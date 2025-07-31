d878518915d97204e5a3f517a6a7cb5d9f3f0188
bajtix <bajtix.b@gmail.com>
2025-07-31 11:59:57 +0200

    fish updates
    Signed-off-by: bajtix <bajtix.b@gmail.com>

--- fish/config.fish
+++ fish/config.fish
@@ -1,23 +1,26 @@
-
 if status is-interactive
-    # Commands to run in interactive sessions can go here
     fastfetch -c examples/8
 end
+
 alias note=/usr/bin/vim
 alias vim=/usr/bin/nvim
+
 set fish_greeting "$(shuf ~/.config/fish/welcomes.txt -n1) | $(date)"
+
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
+# .. but were modified so that the startup does not take ages.
 export CRYPTOGRAPHY_OPENSSL_NO_LEGACY=1
-if test -f /opt/miniconda3/bin/conda
-    eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+#if test -f /opt/miniconda3/bin/conda
+#	# this part took very long for some reason.	
+#	eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+#else
+if test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
+	source "/opt/miniconda3/etc/fish/conf.d/conda.fish"
 else
-    if test -f "/opt/miniconda3/etc/fish/conf.d/conda.fish"
-        . "/opt/miniconda3/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/opt/miniconda3/bin" $PATH
-    end
+	set -x PATH "/opt/miniconda3/bin" $PATH
 end
+#end
 # <<< conda initialize <<<
 
--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,7 +2,8 @@ function fish_prompt
 	set_color green
 	printf '%s' (date "+%H:%M:%S")
 	set_color yellow
-	printf ' %s'(basename $PWD)
+	printf ' %s'(basename $PWD) 
+	printf (fish_vcs_prompt)
 	set_color normal	
 	printf ' $ '
 end
